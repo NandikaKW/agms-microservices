257d0c3a49ceba1de0bb226837bee77917f7a11d
NandikaKW <nandikakaweesha4005@gmail.com>
2026-03-26 16:58:20 +0530

    zoneResponseDto  fields updated and added.

--- AutomationService/src/main/java/lk/ijse/automationservice/dto/ZoneResponseDTO.java
+++ AutomationService/src/main/java/lk/ijse/automationservice/dto/ZoneResponseDTO.java
@@ -10,5 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class ZoneResponseDTO {
+    private int id;
+    private String name;
+    private double minTemp;
+    private double maxTemp;
 
 }
