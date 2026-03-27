bfed358f08b6550b8c166844d115c45f68d8adb1
NandikaKW <nandikakaweesha4005@gmail.com>
2026-03-27 14:56:24 +0530

    api-gateway updated.

--- Api_Gatway/src/main/java/lk/ijse/api_gatway/config/SecurityConfig.java
+++ Api_Gatway/src/main/java/lk/ijse/api_gatway/config/SecurityConfig.java
@@ -6,17 +6,28 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+/*
+    This class is used to configure security for the API Gateway.
+    Here I allow all requests because authentication is handled
+    separately using a custom filter.
+*/
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
         http
-            .csrf(ServerHttpSecurity.CsrfSpec::disable)
-            .authorizeExchange(exchanges -> exchanges
-                .anyExchange().permitAll() // The validation is done in the custom filter
-            );
+                // Disable CSRF since this is mainly for API usage
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+
+                // Allow all requests (custom JWT filter will handle security)
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll()
+                );
+
         return http.build();
     }
 }
+
