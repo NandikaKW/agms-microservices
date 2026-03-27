c8b1ae57ad18d0bd0899dea3e582e407a720efd0
NandikaKW <nandikakaweesha4005@gmail.com>
2026-03-27 14:53:06 +0530

    zone service entry point removed.

--- ZoneService/src/main/java/lk/ijse/zoneservice/ZoneServiceApplication.java
+++ ZoneService/src/main/java/lk/ijse/zoneservice/ZoneServiceApplication.java
@@ -8,8 +8,5 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ZoneServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ZoneServiceApplication.class, args);
-    }
 
 }
