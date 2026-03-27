b109a86e213f492c61b31ad266853392b3db9b83
NandikaKW <nandikakaweesha4005@gmail.com>
2026-03-27 14:44:26 +0530

    new zone-controller added.

--- ZoneService/src/main/java/lk/ijse/zoneservice/controller/ZoneController.java
+++ ZoneService/src/main/java/lk/ijse/zoneservice/controller/ZoneController.java
@@ -13,5 +13,30 @@ import java.util.List;
 @CrossOrigin
 public class ZoneController {
 
+    private final ZoneService service;
 
+    @PostMapping
+    public ZoneDTO save(@RequestBody ZoneDTO dto) {
+        return service.save(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ZoneDTO update(@PathVariable int id, @RequestBody ZoneDTO dto) {
+        return service.update(id, dto);
+    }
+
+    @GetMapping
+    public List<ZoneDTO> all() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ZoneDTO byId(@PathVariable int id) {
+        return service.getById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        service.delete(id);
+    }
 }
