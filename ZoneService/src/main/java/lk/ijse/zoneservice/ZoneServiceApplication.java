8595e4c0f7d28a0b0410c8c1ba0241ea907acca3
NandikaKW <nandikakaweesha4005@gmail.com>
2026-03-27 14:53:41 +0530

    zone service new entry point added.

--- ZoneService/src/main/java/lk/ijse/zoneservice/ZoneServiceApplication.java
+++ ZoneService/src/main/java/lk/ijse/zoneservice/ZoneServiceApplication.java
@@ -8,5 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ZoneServiceApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(ZoneServiceApplication.class, args);
+    }
 
 }
